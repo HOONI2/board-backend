9447082023f1e7cd1bb72c4fdeb20d54087bbfc8
HOON <singeresman@naver.com>
2023-03-01 21:39:31 +0900

    feat : annotation 추가

--- src/main/java/com/example/board/board/mapper/BoardMapper.java
+++ src/main/java/com/example/board/board/mapper/BoardMapper.java
@@ -1,5 +1,8 @@
 package com.example.board.board.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface BoardMapper {
 
 }
---
+++ src/main/java/com/example/board/board/service/impl/BoardServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.board.board.service.impl;
+
+import com.example.board.board.service.BoardService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+}
